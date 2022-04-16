795abdbda31f065f23b4bd710088c05458531c67
Konstantin <kostakv@outlook.com>
2022-04-16 01:18:47 +0000

    [feat]: added typewriter.js

---
+++ hello_timeout.js
@@ -0,0 +1,13 @@
+
+setTimeout(() => {
+
+  console.log('l4545454');
+}, 1000);
+setTimeout(() => {
+
+  console.log('l4545454');
+}, 2000);
+setTimeout(() => {
+
+  console.log('l4545454');
+}, 3000);
