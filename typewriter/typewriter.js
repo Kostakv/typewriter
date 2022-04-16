6a1a5ed2cf8675a328f93022ccab9aff8a125430
Konstantin <kostakv@outlook.com>
2022-04-16 01:20:31 +0000

    [update]: updated typewriter.js

--- typewriter/typewriter.js
+++ typewriter/typewriter.js
@@ -1,16 +1,16 @@
-const sentence = "hello there from lighthouse labs";
 
-let i = 0;
+  const sentence = "hello there from lighthouse labs";
 
-const displayText = function (){
-  process.stdout.write(sentence[i])
-  i++;
-  if (i >= sentence.length){
-    clearInterval(display);
-  }
-}
-
-let display = setInterval(displayText, 500);
+  let i = 0;
   
+  const displayText = function (){
+    process.stdout.write(sentence[i])
+    i++;
+    if (i >= sentence.length){
+      clearInterval(display);
+    }
+  }
+  
+  let display = setInterval(displayText, 500);
   
 
